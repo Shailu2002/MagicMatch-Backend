67931b64d24af08ddb639baa7e5a3553f6c59c69
Shailu2002 <shailusingh955960@gmail.com>
2025-10-31 02:00:42 +0530

    issues fixed

--- database/conn.js
+++ database/conn.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");//import like work
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("connection start")).catch((error) => console.log(error.message));
+mongoose.connect(process.env.DATABASE
+    // ,
+    // {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+    // }
+).then(() => console.log("connection start")).catch((error) => console.log(error.message));

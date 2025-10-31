9a33ea04bd6dfc3777df966602bda19ff59b5721
Shailu2002 <shailusingh955960@gmail.com>
2025-10-31 19:01:46 +0530

    stabilitiy

--- database/conn.js
+++ database/conn.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");//import like work
 // Agar mongoose.connection.readyState 1 ya usse zyada hai, toh connection stable hai.
 if (mongoose.connection.readyState >= 1) {
   console.log('Using existing DB connection (Warm Start)');
-  return; // Agar connected hai, toh naya connection mat banao
 }
-mongoose.connect(process.env.DATABASE
+else {
+  mongoose.connect(process.env.DATABASE
     // ,
     // {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
     // }
-).then(() => console.log("connection start")).catch((error) => console.log(error.message));
+  ).then(() => console.log("connection start")).catch((error) => console.log(error.message));
+}
